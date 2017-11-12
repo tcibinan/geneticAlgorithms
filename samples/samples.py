3f59aa67835ebdccebc64560afdc98b89248ece8
Andrei Tcibin <andrei_tcibin@epam.com>
2017-11-12 12:27:56 +0300

    feat: Add 6 GA tasks

--- samples/samples.py
+++ samples/samples.py
@@ -1,9 +1,51 @@
-from .utils import approximate, float_range
+from math import exp, log
 
-print('y = x')
+from utils import approximate, float_range, min, max, inflection_point, zero
+
+a = 4
+b = 5
+
+print('y = %d*x+%d' % (a, b,))
 approximate(
-    func=lambda x: x,
+    func=lambda x: a * x + b,
     base_range=float_range(-2.0, 2.0, 0.5),
     bounds=(-10.0, 10.0,),
     population_size=300,
     generations=50)
+
+print('y = %d*exp(x*%f)' % (a, b,))
+approximate(
+    func=lambda x: a * exp(x * b),
+    base_range=float_range(-2.0, 2.0, 0.5),
+    bounds=(-10000.0, 10000.0,),
+    population_size=500,
+    generations=50)
+
+min(
+    func=lambda x: (x-0.8)**2 + 4,
+    search_region=(-2.0, 2.0,),
+    population_size=300,
+    generations=50
+)
+
+max(
+    func=lambda x: 1.0/x,
+    search_region=(-4.0, -0.0,),
+    population_size=300,
+    generations=50
+)
+
+inflection_point(
+    func=lambda x: (x - 1.5) ** 3 + 3,
+    search_region=(-10.0, 10.0,),
+    population_size=300,
+    generations=50
+)
+
+zero(
+    func=lambda x: log(x + 1) - 2.25,
+    search_region=(-0.5, 15.0,),
+    population_size=300,
+    generations=50
+)
+
--- samples/utils.py
+++ samples/utils.py
@@ -12,8 +12,8 @@ def approximate(func, base_range, bounds, population_size, generations):
             score += abs(func(x) - individual[idx])
         return score,
 
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMin)
+    creator.create("Fitness", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", list, fitness=creator.Fitness)
 
     toolbox = base.Toolbox()
 
@@ -38,6 +38,96 @@ def approximate(func, base_range, bounds, population_size, generations):
     show_best_individual(base_range, func, population)
 
 
+def min(func, search_region, population_size, generations, invert=False):
+    creator.create("Fitness", base.Fitness, weights=(1.0 if invert else -1.0,))
+    creator.create("Individual", list, fitness=creator.Fitness)
+
+    toolbox = base.Toolbox()
+
+    toolbox.register("attr_bool", float_between, *search_region)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=1)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    toolbox.register("evaluate", lambda individual: (func(individual[0]),))
+    toolbox.register("mate", skippingCrosover)
+    (a, b) = search_region
+    toolbox.register("mutate", mutInRegion, a=a, b=b, max_step=0.1, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    population = toolbox.population(n=population_size)
+
+    for gen in range(generations):
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+        population = toolbox.select(offspring, k=len(population))
+
+    print(tools.selBest(population, k=1))
+
+    base_range = float_range(*search_region)
+    plot(base_range, [func(x) for x in base_range], 'r')
+    show()
+
+
+def inflection_point(func, search_region, population_size, generations):
+    geneticAlgorithmWithEvalFunctional(func, evaluate_diff, search_region, population_size, generations,
+                                       func=func,
+                                       step=0.001)
+
+
+def zero(func, search_region, population_size, generations):
+    geneticAlgorithmWithEvalFunctional(func, evaluate_zero, search_region, population_size, generations,
+                                       func=func)
+
+
+def evaluate_zero(individual, func):
+    return abs(func(individual[0])),
+
+
+def geneticAlgorithmWithEvalFunctional(base_func, eval_func, search_region, population_size, generations, **args):
+    creator.create("Fitness", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", list, fitness=creator.Fitness)
+
+    toolbox = base.Toolbox()
+
+    toolbox.register("attr_bool", float_between, *search_region)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=1)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    toolbox.register("evaluate", eval_func, **args)
+    toolbox.register("mate", skippingCrosover)
+    (a, b) = search_region
+    toolbox.register("mutate", mutInRegion, a=a, b=b, max_step=0.1, indpb=0.05)
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    population = toolbox.population(n=population_size)
+
+    for gen in range(generations):
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+        population = toolbox.select(offspring, k=len(population))
+
+    print(tools.selBest(population, k=1))
+
+    base_range = float_range(*search_region)
+    plot(base_range, [base_func(x) for x in base_range], 'r')
+    show()
+
+
+def evaluate_diff(individual, func, step):
+    y = func(individual[0])
+    left = abs(y - func(individual[0] - step))
+    right = abs(y - func(individual[0] + step))
+    return (right if right < left else left),
+
+
+def max(func, search_region, population_size, generations):
+    min(func, search_region, population_size, generations, invert=True)
+
+
 def show_best_individual(base_range, func, population):
     best_individual = tools.selBest(population, k=1)[0]
     plot(base_range, best_individual, 'g')
@@ -51,3 +141,25 @@ def float_between(lower_bound, upper_bound):
 
 def float_range(a, b, step=0.5):
     return np.arange(a, b, step)
+
+
+def skippingCrosover(ind1, ind2):
+    return (ind1, ind2,)
+
+
+def getMu(a, b):
+    return (a + b) / 2
+
+
+def mutInRegion(individual, a, b, max_step, indpb):
+    step = random.random() * max_step
+
+    if random.random() < indpb:
+        if random.random() > 0.5:
+            if individual[0] + step < b:
+                individual[0] += step
+        else:
+            if individual[0] - step > a:
+                individual[0] -= step
+
+    return individual,
